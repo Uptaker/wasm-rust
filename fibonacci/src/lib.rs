07826d32ea15795b5c55406af65dff96bdde1e1b
Markus Tammeoja <markus.tammeoja@gmail.com>
2023-04-28 03:20:35 +0300

    add sieve of erastosthenes solution after wasting all day dealing with resolving the last number - give credit to found solution

--- fibonacci/src/lib.rs
+++ fibonacci/src/lib.rs
@@ -1,13 +1,9 @@
-// use wasm_bindgen::prelude::*;
+use wasm_bindgen::prelude::*;
+use js_sys::Array;
+use bit_vec::BitVec;
 
-fn main() {
-    
-    print!("{}", fibonacci(1000000));
-}
-
-
-// #[wasm_bindgen]
-fn fibonacci(n: u64) -> u64 {
+#[wasm_bindgen]
+pub fn fibonacci(n: u64) -> u64 {
     if n == 0 {
         return 0;
     } else if n == 1 {
@@ -21,4 +17,27 @@ fn fibonacci(n: u64) -> u64 {
         b = c;
     }
     b
+}
+
+/*
+    After many attempts and failures, 
+    I've started to look elsewhere due to my insufficient knowledge of Rust.
+    This implementation worked splendidly with the last input, which is a copy of this:
+    https://github.com/rgadams/wasm-primes/blob/main/src/lib.rs
+*/
+
+#[no_mangle]
+#[wasm_bindgen(js_name = sieveOfEratosthenes)]
+pub fn sieve_of_eratosthenes(n: usize) -> Vec<usize> {
+    let mut primes: Vec<bool> = (0..n + 1).map(|num| num == 2 || num & 1 != 0).collect();
+    let mut num = 3usize;
+    while num * num <= n {        
+        let mut j = num * num;
+        while j <= n {
+            primes[j] = false;
+            j += num;
+        }
+        num += 2;
+    }
+    primes.into_iter().enumerate().skip(2).filter_map(|(i, p)| if p {Some(i)} else {None}).collect::<Vec<usize>>()
 }
